8eb89c16c4e06df94dd0ec8658c3306fb55b06ba
Kashif Rezwi <kashifrezwi850@gmail.com>
2026-06-04 05:38:48 +0530

    feat: integrate Prisma ORM and setup PrismaService module in NestJS backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    PrismaModule,
   ],
   controllers: [HealthController],
 })
